1ffbacc85b9bd8a60f09258d2f803b517a8a530c
alizarion <selim@openlinux.fr>
2016-02-28 17:36:47 +0100

    Adding springboot package

---
+++ springboot-package/src/main/java/io/github/alizarion/Application.java
@@ -0,0 +1,44 @@
+package io.github.alizarion;
+
+import io.github.alizarion.common.api.PersonRessource;
+import io.github.alizarion.common.tools.web.filters.AccessControlAllowOriginFilter;
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import javax.inject.Named;
+
+/**
+ * @author selim@openlinux.fr.
+ */
+@SpringBootApplication
+public class Application {
+
+    @Named
+    public static class JerseyConfig extends ResourceConfig {
+
+        public JerseyConfig() {
+            this.register(PersonRessource.class);
+
+            this.register(JacksonFeature.class);
+        }
+    }
+
+    @Bean
+    public FilterRegistrationBean contextFilterRegistrationBean() {
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        AccessControlAllowOriginFilter contextFilter = new AccessControlAllowOriginFilter();
+        registrationBean.setFilter(contextFilter);
+        registrationBean.setOrder(1);
+        return registrationBean;
+    }
+
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
